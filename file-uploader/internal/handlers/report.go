a35124a0484e6631f6a8e22c1085b328c662d6ed
Sergey <landyrev.serg@gmail.com>
2024-04-15 19:27:37 +0300

    post

--- file-uploader/internal/handlers/report.go
+++ file-uploader/internal/handlers/report.go
@@ -56,46 +56,58 @@ func buildHandlerReport(db *sql.DB) func(c *gin.Context) {
 
 			log.Print("here")
 
+			type row struct {
+				id          string
+				filename    string
+				send_date   time.Time
+				upload_date time.Time
+				to          string
+				cc          string
+				from        string
+				subject     string
+				content     string
+			}
+			rowC := make(chan *row, 20)
+
 			wg := &sync.WaitGroup{}
+			wg.Add(1)
+
+			go func() {
+				defer wg.Done()
+				for rows.Next() {
+					var r row
+					rows.Scan(
+						&r.id,
+						&r.filename,
+						&r.send_date,
+						&r.upload_date,
+						&r.to,
+						&r.cc,
+						&r.from,
+						&r.subject,
+						&r.content,
+					)
+
+					rowC <- &r
+				}
+			}()
+
 			for i := 0; i < 20; i++ {
 				wg.Add(1)
 				go func() {
 					defer wg.Done()
-					for rows.Next() {
-						var (
-							id          string
-							filename    string
-							send_date   time.Time
-							upload_date time.Time
-							to          string
-							cc          string
-							from        string
-							subject     string
-							content     string
-						)
-
-						rows.Scan(
-							&id,
-							&filename,
-							&send_date,
-							&upload_date,
-							&to,
-							&cc,
-							&from,
-							&subject,
-							&content,
-						)
+					for cur, ok := <-rowC; ok; {
 
 						dangerValues := make([]string, 0)
 
 						for _, r := range cr {
-							dangerValues = append(dangerValues, r.FindAllString(string(content), -1)...)
+							dangerValues = append(dangerValues, r.FindAllString(string(cur.content), -1)...)
 						}
 
 						if len(dangerValues) > 0 {
 							items <- gin.H{
-								"id":   id,
-								"name": filename,
+								"id":   cur.id,
+								"name": cur.filename,
 							}
 						}
 					}
